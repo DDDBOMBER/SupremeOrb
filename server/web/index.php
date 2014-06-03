1a8e0015bbc803d9194554b38d05495c46a60080
DDDBOMBER <dylandhokia@gmail.com>
2014-06-03 10:37:58 +0100

    Added Base for Website

---
+++ server/web/index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Supreme Orb - The online competitive game</title>
+		<link rel="stylesheet" type="text/css" href="/style.css?<?php echo time();?>">
+	</head>
+	<body>
+
+	</body>
+	<script type="text/javascript" src="script.js"></script>
+</html>
