c5944d6e52dd55212cbb5388027f50dd2b935335
DDDBOMBER <dylandhokia@gmail.com>
2014-06-03 10:20:31 +0100

    Started Multiplayer System

---
+++ app/src/com/dddbomber/proton/GameConnection.java
@@ -0,0 +1,36 @@
+package com.dddbomber.proton;
+
+import org.net.Msg.*;
+import org.net.Msg.Msg;
+import org.net.p2p.*;
+
+import com.dddbomber.proton.entity.Colors;
+
+public class GameConnection {
+	public Colors opponentColor;
+	
+	public GameConnection(String ip, int port){
+		Protocol p = new Protocol(this);
+		p.addMsgHandler("up", "update");
+		jnmp2p jnm = new jnmp2p(p, port);
+		Connection c = jnm.connect(ip);
+		
+		sendMsg(c);
+	}
+	
+	public void update(Connection c, Msg m){
+		System.out.println(((Colors)m.getContent()));
+		sendMsg(c);
+	}
+	
+	public void sendMsg(Connection c){
+		String head = "up";
+		Colors body = Colors.cols().get(5);
+		Msg m = c.createMsg(head, body);
+		c.sendMsg(m);
+	}
+	
+	public static void main(String[] args){
+		GameConnection gc = new GameConnection("localhost", 12000);
+	}
+}
