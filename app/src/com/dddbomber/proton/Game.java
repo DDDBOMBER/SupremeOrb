f772366e5487f54eef0df2b556dbe0b6ddf99f4a
DDDBOMBER <dylandhokia@gmail.com>
2014-06-03 10:17:12 +0100

    Added Game Base

---
+++ app/src/com/dddbomber/proton/Game.java
@@ -0,0 +1,199 @@
+package com.dddbomber.proton;
+
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.HeadlessException;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import net.java.games.input.AbstractComponent;
+import net.java.games.input.Component;
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
+import kuusisto.tinysound.Music;
+import kuusisto.tinysound.Sound;
+import kuusisto.tinysound.TinySound;
+
+import com.dddbomber.proton.assets.Asset;
+import com.dddbomber.proton.assets.Screen;
+import com.dddbomber.proton.input.InputHandler;
+import com.dddbomber.proton.menu.Menu;
+
+public class Game extends Canvas implements Runnable{
+	public static int tickRate = 64;
+	
+	private static final long serialVersionUID = 1L;
+	//public static int WIDTH = 1138, HEIGHT = 640;
+	public static int WIDTH = 960, HEIGHT = 640;
+	public static int SCREENWIDTH = 480, SCREENHEIGHT = 320;
+	public static final String NAME = "Red Proton";
+	
+	public Screen screen;
+	public InputHandler input;
+	
+	public static Music ts;
+	public static Sound hit;
+	
+	public Game(){
+		try {
+			TinySound.init();
+			hit = TinySound.loadSound("/hit.wav");
+			ts = TinySound.loadMusic("/Quadratic.wav");
+			//ts.play(true);
+			//setVolume(0.15);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			setCursor(Toolkit.getDefaultToolkit().createCustomCursor(ImageIO.read(Game.class.getResourceAsStream("/cursor.png")), new Point(0, 0), "invisible"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		setSize(new Dimension(WIDTH, HEIGHT));
+		screen = new Screen(SCREENWIDTH, SCREENHEIGHT);
+		screen.fill(0, 0, screen.width, screen.height, 0x2A1D24);
+		input = new InputHandler(this);
+	}
+	
+	public static void setVolume(double volume){
+		ts.setVolume(volume);
+	}
+	
+	public static int ticks, renders;
+	
+	public void run() {
+		long lastTime = System.nanoTime();
+		double nsPerTick = 1000000000.0 / 60.0;
+		
+		double time = 0;
+		
+		long lastSecond = System.currentTimeMillis();
+		
+		this.requestFocus();
+		
+		while(running){
+			long now = System.nanoTime();
+			time += (now - lastTime) / nsPerTick;
+			lastTime = now;
+			boolean render = false;
+			while(time >= 1){
+				tick();
+				time -= 1;
+				render = true;
+			}
+			if(render){
+				render();
+			}else{
+				try {
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					
+				}
+			}
+			if(System.currentTimeMillis() - lastSecond > 1000){
+				lastSecond += 1000;
+				fps = renders;
+				ticks = renders = 0;
+				System.out.println("FPS - "+fps);
+			}
+		}
+	}
+	
+	public static int fps;
+	
+	private void render() {
+		renders++;
+
+		WIDTH = getWidth();
+		HEIGHT = getHeight();
+		if(SCREENWIDTH != WIDTH/2 || SCREENHEIGHT != HEIGHT/2){
+			screen = new Screen(SCREENWIDTH=WIDTH/2, SCREENHEIGHT=HEIGHT/2);
+		}
+		
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null){
+			this.createBufferStrategy(2);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		Menu.menu.render(screen);
+		
+		g.drawImage(screen.getImage(), 0, 0, getWidth(), getHeight(), null);
+
+		g.dispose();
+		bs.show();
+	}
+	
+	public boolean pressed = false;
+	
+	Random random = new Random();
+
+	private void tick() {
+		ticks++;
+		if(!input.focus.has)return;
+		Menu.menu.tick(input);
+		if(input.keyboard.keys[KeyEvent.VK_ESCAPE]){
+			System.exit(0);
+		}
+		//gpc.poll();
+	}
+
+	public static Image icon;
+
+	public static JFrame frame;
+	
+	public static void main(String[] args){
+		
+		Asset.loadAssets();
+		Game game = new Game();
+		frame = new JFrame(NAME);
+		try{
+			icon = ImageIO.read(Game.class.getResourceAsStream("/icon.png"));
+			frame.setIconImage(icon);
+		}catch(Exception e){
+			
+		}
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.add(game, BorderLayout.CENTER);
+		frame.setContentPane(panel);
+		frame.pack();
+		frame.setResizable(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+	
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		
+		game.start();
+	}
+
+	private Thread gameThread;
+
+	public boolean running = false;
+
+	public void start() {
+		running = true;
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+
+	public void stop() {
+		running = false;
+	}
+}
