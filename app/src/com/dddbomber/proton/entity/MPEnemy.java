481684467381ab74df094f23da3211cc0bd9426f
DDDBOMBER <dylandhokia@gmail.com>
2014-06-04 19:39:13 +0100

    Added MPEnemy and MPLevel

---
+++ app/src/com/dddbomber/proton/entity/MPEnemy.java
@@ -0,0 +1,74 @@
+package com.dddbomber.proton.entity;
+
+import com.dddbomber.proton.assets.Screen;
+import com.dddbomber.proton.cpu.NameGenerator;
+import com.dddbomber.proton.input.InputHandler;
+import com.dddbomber.proton.level.Level;
+
+public class MPEnemy extends Enemy {
+
+	public boolean walking, left;
+	public int anim, shootDelay;
+	
+	public int col, light;
+	
+	public MPEnemy(int x, int y, Colors colors, String name){
+		super(x, y);
+		this.colors = colors;
+		this.name = name;
+		col = colors.col;
+		light = colors.light;
+	}
+	
+	@Override
+	public void tick(Level level, InputHandler input) {
+		
+		if(ySpeed > 0.5)ySpeed -= 0.5;
+		else if(ySpeed < -0.5)ySpeed += 0.5;
+		else ySpeed = 0;
+		
+		if(xSpeed > 0.5)xSpeed -= 0.5;
+		else if(xSpeed < -0.5)xSpeed += 0.5;
+		else xSpeed = 0;
+		
+		x += xSpeed;
+		y += ySpeed;
+		
+		if(xSpeed < -4)xSpeed = -4;
+		if(xSpeed > 4)xSpeed = 4;
+		if(ySpeed < -4)ySpeed = -4;
+		if(ySpeed > 4)ySpeed = 4;
+		
+		if(shootDelay > 0)shootDelay--;
+		if(shootDelay == 0){
+			shootDelay = 20;
+			level.entities.add(new Bullet(colors, x+2, y+2, Math.toDegrees(Math.atan2(level.player.y-y+level.player.ySpeed*5, level.player.x-x+level.player.xSpeed*5))+90, size, this));
+			double rot = Math.toRadians(Math.toDegrees(Math.atan2(level.player.y-y+level.player.ySpeed*5, level.player.x-x+level.player.xSpeed*5))+90);
+			x -= Math.sin(rot)*1;
+			y += Math.cos(rot)*1;
+		}
+		
+		xSize = ySize = size;
+		if(hurtTime > 0)hurtTime--;
+
+		
+		if(removed){
+			level.slowDown = 36;
+			for(int i = 0; i < 75; i++){
+				Particle p = new Particle(colors, x, y, random.nextInt(360), 8);
+				p.tick(level, input);
+				level.entities.add(p);
+			}
+		}
+	}
+
+	public void render(Level level, Screen screen, int xScroll, int yScroll) {
+		if(hurtTime > 0){
+			screen.renderLight((int)x-xScroll, (int)y-yScroll, size+shootDelay/2, hurtColor.light);
+		}
+		screen.renderFaint((int)x-xScroll, (int)y-yScroll, size*4, col);
+		screen.renderLight((int)x-xScroll, (int)y-yScroll, size+shootDelay/2, col);
+		screen.renderLight((int)x-xScroll, (int)y-yScroll, size+shootDelay/2, light);
+	}
+
+}
---
+++ app/src/com/dddbomber/proton/level/MPLevel.java
@@ -0,0 +1,121 @@
+package com.dddbomber.proton.level;
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Random;
+
+import com.dddbomber.proton.Game;
+import com.dddbomber.proton.assets.Asset;
+import com.dddbomber.proton.assets.Screen;
+import com.dddbomber.proton.entity.Colors;
+import com.dddbomber.proton.entity.Enemy;
+import com.dddbomber.proton.entity.Entity;
+import com.dddbomber.proton.entity.EntitySpawner;
+import com.dddbomber.proton.entity.NamedEntity;
+import com.dddbomber.proton.entity.Player;
+import com.dddbomber.proton.input.InputHandler;
+import com.dddbomber.proton.menu.ColorSelectMenu;
+import com.dddbomber.proton.menu.GainInEloMenu;
+import com.dddbomber.proton.menu.Menu;
+
+public class MPLevel extends Level{
+	public final int opponentRank;
+	public final Colors opponentColors;
+	
+	public MPLevel(Colors color, Colors opponentColors, int opponentRank){
+		super(color);
+		this.opponentColors = opponentColors;
+		this.opponentRank = opponentRank;
+	}
+	
+	public void render(Screen screen){
+		if(countdown < 61 && countdown > 0){
+			screen.draw("Go", screen.width/2-8, 64, 0xffffff, 2);
+		}else if(countdown >= 60){
+			screen.draw(""+(countdown)/60, screen.width/2-6, 96, 0xffffff, 2);
+		}
+
+		if(player.removed || (enemies < 1 && countdown == 0)){
+			if(restartDelay-- <= 0){
+				String msg = "";
+				if(won == 1)msg = "You Won";
+				if(won == 2)msg = "You Lose";
+				if(won == 3)msg = "You Draw";
+				screen.draw(msg, screen.width/2-msg.length()*6, 64, 0xffffff, 2);
+				screen.draw("Press Space To Restart", screen.width/2-132, 96, 0xffffff, 2);
+			}
+		}
+		if(slowDown > 0 && ticks % 2 == 0){
+			//Skip a tick
+		}else{
+			if(back == -1){
+				int o1 = 0x2E2128;
+				int o2 = screen.merge(o1, player.col, 80);
+				int o3 = screen.merge(o2, enemy.col, 80);
+				int o4 = screen.merge(o2, 0, 85);
+				back = o4;
+			}
+			screen.fill(0, 0, screen.width, screen.height, back, 20);
+			for(Entity e : entities){
+				e.render(this, screen, xScroll, yScroll);
+			}
+			
+		}
+		int offset = 8;
+		for(Entity e : entities){
+			if(e instanceof Enemy || e instanceof Player){
+				renderOverlayForEntity(screen, offset, (NamedEntity)e);
+				renderRank(screen, offset, e);
+				offset = screen.width-132;
+			}
+		}
+	}
+
+
+	public void renderRank(Screen screen, int offset, Entity e){
+		screen.drawTrans(Asset.rank_overlay, offset-4, screen.height-36, 0, 0, 32, 32, 50);
+		screen.drawTrans(Asset.ranks[(e == player ? 9 : 13)], offset, screen.height-32, 0, 0, 24, 24, 50);
+	}
+	
+	public void renderOverlayForEntity(Screen screen, int offset, NamedEntity player){
+		screen.fill(offset+28, screen.height-32, 96, 24, player.colors.col, 10);
+
+		screen.draw(""+player.health, offset+28, screen.height-31, player.colors.light, 2);
+		screen.fill(offset+28, screen.height-16, 96, 8, player.colors.col);
+		screen.fill(offset+28, screen.height-16, player.health*3, 8, player.colors.light);
+		
+		screen.draw(player.name, offset+52, screen.height-31, player.colors.light, 1);
+	}
+	
+	public void tick(InputHandler input){
+		if(countdown > 0)countdown--;
+		ticks++;
+		slowDown--;
+		if(slowDown > 0 && ticks % 2 == 0){
+			return;
+		}
+		enemies = 0;
+		for(int i = 0; i < entities.size(); i++){
+			Entity e = entities.get(i);
+			e.tick(this, input);
+			if(e.removed){
+				entities.remove(i--);
+			}else{
+				if(e instanceof Enemy || (e instanceof EntitySpawner && ((EntitySpawner)e).entityToSpawn instanceof Enemy))enemies++;
+			}
+		}
+		if(player.removed || (enemies < 1 && countdown == 0)){
+			if(player.removed){
+				won = 2;
+				if(enemies < 1)won = 3;
+			}else{
+				won = 1;
+			}
+			if(restartDelay-- <= 0){
+				if(input.keyboard.keys[KeyEvent.VK_SPACE]){
+					Menu.menu = new ColorSelectMenu();
+				}
+			}
+		}
+	}
+}
