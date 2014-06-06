6505423e543acfb29d1d0f28bf56e184162f00e2
DDDBOMBER <dylandhokia@gmail.com>
2014-06-06 09:26:31 +0100

    Finished Creating Of Users

--- server/user/create_user.php
+++ server/user/create_user.php
@@ -1,5 +1,22 @@
 <?php 
 	
+	require_once "/lib/meekrodb.2.2.class.php";
+
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+
+	if($username && $password){
+		$salt = randomSalt();
+		$hashPassword = hashPassword($password, $salt);
+		$userKey = randomSalt(64);
+
+		DB::insert("UserLogon", array(
+			"UserName" => $username,
+			"UserSalt" => $salt,
+			"UserPassword" => $hashPassword,
+			"UserKey" => $userKey
+		));
+	}
 
 	function randomSalt($len = 8) {
 		$chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`~!@#$%^&*()-=_+';
@@ -9,6 +26,14 @@
 			$str .= $chars[rand(0, $l];
 	 	}
 		return $str;
+	}
+
+	function hashPassword($password, $salt){
+		$hash = "";
+		for ($i=0; $i < 2206; $i++) { 
+			$hash = sha1($hash.$password.$salt);
+		}
+		return $hash;
 	}
 	
  ?>
