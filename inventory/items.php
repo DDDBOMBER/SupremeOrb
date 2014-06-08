837eb4d9b65b8f9982c0bd164e7390794680b775
DDDBOMBER <dylandhokia@gmail.com>
2014-06-08 15:32:01 +0100

    Added All Colors To Items.php

--- inventory/items.php
+++ inventory/items.php
@@ -16,29 +16,33 @@
 
 	function get_color_list(){
 		$a = array(
-			new Color("8B0800", "C04424"),
-			new Color("4D712B", "B6B929"),
-			new Color("2E70D1", "6DC2CA"),
-
-			new Color("D27D2C", "DAD45E"),
-			new Color("90E6FF", "FFFFFF"),
-			new Color("9244ff", "EA00C4"),
-
-			new Color("FB7C5B", "FDCDC1"),
-			new Color("C7D93D", "E9F2A0"),
-			new Color("4B374C", "B29ABB"),
-			
-			new Color("3D3D3D", "606060"),
-			new Color("287256", "CEFF9E"),
-			new Color("7A374E", "D27D2C"),
-			
-			new Color("341B8E","685E8E"),
-			new Color("504C40", "8C4A3D"),
-			new Color("54403D", "D9BD79"),
-			
-			new Color("2C729E", "8C2E2E"),
-			new Color("712F14", "BCD074"),
-			new Color("685E8E", "A4616D"),
+			new Color("4929356", "11705019"),
+			new Color("4013373", "12369084"),
+			new Color("5262400", "9194045"),
+			new Color("5521469", "14269817"),
+			new Color("7417620", "12374132"),
+			new Color("6839950", "10772845"),
+			new Color("4340574", "9453381"),
+			new Color("4866368", "1477251"),
+			new Color("8426345", "8426345"),
+			new Color("3414926", "6839950"),
+			new Color("8564358", "16777215"),
+			new Color("10180517", "16777215"),
+			new Color("13794604", "14341214"),
+			new Color("9496319", "16777215"),
+			new Color("9111552", "12600356"),
+			new Color("13795158", "16777215"),
+			new Color("16481371", "16633281"),
+			new Color("13097277", "15332000"),
+			new Color("6839950", "15520205"),
+			new Color("10565988", "9334857"),
+			new Color("5075243", "11974953"),
+			new Color("2912926", "9186929"),
+			new Color("317913", "15912806"),
+			new Color("3043537", "7193290"),
+			new Color("2650710", "13565854"),
+			new Color("8009550", "13794604"),
+			new Color("9585919", "15335620"),
 		);
 
 		return $a;
