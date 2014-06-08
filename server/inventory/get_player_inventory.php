ba12d3ed786621a466e8664f606afd84ab791a8b
DDDBOMBER <dylandhokia@gmail.com>
2014-06-08 19:59:57 +0100

    Added Getting Of Inventory

--- server/inventory/get_player_inventory.php
+++ server/inventory/get_player_inventory.php
@@ -1,3 +1,8 @@
 <?php 
-	$id = $_POST["id"];
+	require_once "../lib/meekrodb.2.2.class.php";
+
+	$id = $_GET["id"];
+	$results = DB::query("SELECT * FROM UserItems WHERE UserID=%i", $id);
+	$string = json_encode($results);
+	echo $string;
 ?>	
