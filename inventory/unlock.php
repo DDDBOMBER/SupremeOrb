44160c82ffafc43e98024d6a9b3cf825494b2ecf
DDDBOMBER <dylandhokia@gmail.com>
2014-06-08 15:36:21 +0100

    Fixed unlock.php

--- inventory/unlock.php
+++ inventory/unlock.php
@@ -35,13 +35,13 @@
 
 				if($second_seed < 4){
 					// Common
-					$col[] = $a[$third_seed];
-				}else if($second_seed < 7){
+					$cols[] = $a[$third_seed];
+				}else if($second_seed < 8){
 					// Mid
-					$col[] = $a[$third_seed+9];
-				}else if($second_seed < 9){
+					$cols[] = $a[$third_seed+9];
+				}else if($second_seed < 10){
 					// Rare
-					$col[] = $a[$third_seed+18];
+					$cols[] = $a[$third_seed+18];
 				}
 
 				$item->metadata = $cols;
@@ -60,18 +60,20 @@
 			$second_seed = rand(1,9);
 			$third_seed = rand(0,8);
 
-			if($second_seed < 4){
+			if($second_seed < 5){
 				// Common
 				$item->metadata = $a[$third_seed];
-			}else if($second_seed < 7){
+			}else if($second_seed < 8){
 				// Mid
 				$item->metadata = $a[$third_seed+9];
-			}else if($second_seed < 9){
+			}else if($second_seed < 10){
 				// Rare
 				$item->metadata = $a[$third_seed+18];
 			}
 
 		}
+
+		return $item;
 	}
 
 	echo "<pre>";
